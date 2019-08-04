e08eb11867294b54a05ffddd5fac2b299ca244f8
sajjad <af.hadafi@gmail.com>
2019-08-04 10:39:16 +0430

    change deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,13 @@ docker build -t afsan007/multi-client:latest -t afsan007/multi-client:$SHA -f ./
 docker build -t afsan007/multi-server:latest -t afsan007/multi-server:$SHA -f ./server/Dockerfile ./server
 docker build -t afsan007/multi-worker:latest -t afsan007/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
-docker push afsan007/multi-client
-docker push afsan007/multi-server
-docker push afsan007/multi-worker
+docker push afsan007/multi-client:latest
+docker push afsan007/multi-server:latest
+docker push afsan007/multi-worker:latest
+
+docker push afsan007/multi-client:$SHA
+docker push afsan007/multi-server:$SHA
+docker push afsan007/multi-worker:$SHA
 
 docker apply -f k8s/client
 docker apply -f k8s/database
